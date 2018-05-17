bc37177149fc4b60975f417ace05c3d63a96b6c2
bbailey21us <brandonbailey21@gmail.com>
2018-05-17 15:49:55 -0400

    added some JS

--- app.js
+++ app.js
@@ -12,5 +12,31 @@ var brandon={
 
 };
 
-console.log(brandon);
+var pedro={
+    speed:8,
+    sleep:2,
+    nationality:"mexico",
+    address:{
+        address1:"2190 Seasons north Drive",
+        address2:"unit 210",
+        city:"Juarez",
+        zip:"46280"
+
+    }
+
+};
+
+var answer=prompt("what is your name?")
+
+if (answer.toUpperCase()==="BRANDON"){
+   console.log(brandon);
+   alert(brandon.nationality);
+} 
+else if(answer.toUpperCase()==="PEDRO"){
+    console.log(pedro);
+}
+else{
+    alert("get out")
+};
+
 
