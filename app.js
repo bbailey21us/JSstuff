08ae794af394df0bc355962915e8ecf10e19a7c6
bbailey21us <brandonbailey21@gmail.com>
2018-05-17 14:42:21 -0400

    small change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+let fruit=['bill','bob', 'jon']
+
+
+console.log(fruit);
+fruit.push('pete');
+console.log(fruit);
+console.log(fruit.pop());
+console.log(fruit);
+
+var brandon={
+    speed:10,
+    sleep:5,
+    nationality:"american"
+
+};
+
+console.log(brandon);
+alert(brandon.speed);
